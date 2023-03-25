84e6fbcddad9f04642b9ff87576ec5ef186d986a
JayJohn2020 <74708403+JayJohn2020@users.noreply.github.com>
2023-03-25 11:10:17 +0000

    Update streamlit_app.py
    corrected body error to header2

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.body('Omega 3 & Blueberry Oatmeal')
+streamlit.header2('Omega 3 & Blueberry Oatmeal')
